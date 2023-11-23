b903f87e425c41ed25aa269330b1a3d2a625d672
akshatcoder-hash <akshatcoder@outlook.com>
2023-11-24 01:15:24 +0530

    minting tokens

--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -9,21 +9,25 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-const token_decimals = 1_000_000n;
+const token_decimals = 1_000_00;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("7VuA969pmH8nHypWqF9Dpyzt8Fsj5U6PSKdZbcQoDP6J"); // This is the mint address for the token we created in the previous step
 
 (async () => {
     try {
         // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
 
         // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair, 2*token_decimals, undefined);
+        console.log(`Your mint txid: ${mintTx}`);
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
+
+// Output:
+// Your ata is: 9QWHR5MthPxqNNTmPfKUuTMRgwvkxMs12RFxAWNsPC7p
+// Your mint txid: 5A1Yi2Z5BUCWP8EP7hBQzCiWG4583AhoJue3aDim9NjRGxiVdJoLvyjdAaYwp6GVGinJxCntMELJJew7pmeC9yxc
